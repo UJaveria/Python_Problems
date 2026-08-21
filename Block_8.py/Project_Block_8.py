734b4f6abbfd66302cf96b17247d0854ecd93222
Javeria <umejaveria12@gmail.com>
2026-08-21 11:02:14 +0500

    Project is modified, due to logic mistak

--- Block_8.py/Project_Block_8.py
+++ Block_8.py/Project_Block_8.py
@@ -6,23 +6,22 @@ AND a onesentence feedback message that differs by grade
 Handle invalid input (negative numbers or over 100) with its own message"""
 
 score = int(input("Enter a score(1-100) : "))
-if score <= 100 :
-    if  score > 90  :
+
+if score <= 100 and score >= 0:
+    if  score >= 90  :
         print("A")
         print("Excellent work!")
-    elif score > 70 :
+    elif score >= 70 :
         print("B")
         print("V.Good work!")
-    elif score > 50 :
+    elif score >= 50 :
         print("C")
         print("Good work!")
-    elif score > 30 :
+    elif score >= 30 :
         print("D")
         print("Poor work!")
-    elif score > 0 :
+    else :
         print("F")
         print("Let's set up extra practice time.")
-    else :
-        print("Enter positive score!")
 else :
     print("Invalid input")
